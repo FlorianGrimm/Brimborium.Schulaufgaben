6a987c45a5dd0c8c8f9db61853293a494c984172
Florian Grimm <grimmuko@gmail.com>
2025-11-20 00:32:07 +0100

    backend

---
+++ src/backend/SchulaufgabenClientWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SchulaufgabenClientWeb.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
